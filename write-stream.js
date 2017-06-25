9c588f4c7cdd34910f798e2159a5626cb69a8b96
Jasmeet Singh <jascee.gtbit@gmail.com>
2017-06-25 23:39:50 +0530

    Write Stream

---
+++ write-stream.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt' , 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+
+myReadStream.on('data' , function(chunk){
+	console.log('new chunk Recieved');
+	myWriteStream.write(chunk);
+});
